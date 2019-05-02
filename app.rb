d40e4908f7f391f1303c9caed8848537b711923f
Joy Lin <vvlnote@hotmail.com>
2019-05-02 00:36:20 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,24 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
+  configure do
+    enable :sessions
+    set :session_secret, "secret"
+  end
+  
+  get '/' do
+    
+    erb :index
+  end
+  
+  post '/checkout' do
+    puts params
+    session[:item] = params[:item]
+    @session = session
+    puts @session
+    #binding.pry
+    erb :checkout
+  end
+  
 end
